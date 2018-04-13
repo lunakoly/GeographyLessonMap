6bd45d72e06ac05966322c83b312fe2c6e1ae412
Nick <lunyak.kolya@mail.ru>
2018-04-14 00:09:36 +0300

    edit

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ google.charts.load('current', {
     'packages': ['geochart'],
     // Note: you will need to get a mapsApiKey for your project.
     // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    // Hopefully, you won't stoll it, right?
     'mapsApiKey': 'AIzaSyBuZPXmiSUiYc-GjrTXKl9cevtgIVjv8Xw'
 })
 
