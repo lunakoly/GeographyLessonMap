ac168965cd02eba3d9c9ea1874e4da150cacefc3
Nick <lunyak.kolya@mail.ru>
2018-04-14 00:04:26 +0300

    init

---
+++ script.js
@@ -0,0 +1,45 @@
+google.charts.load('current', {
+    'packages': ['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyBuZPXmiSUiYc-GjrTXKl9cevtgIVjv8Xw'
+})
+
+google.charts.setOnLoadCallback(drawRegionsMap)
+
+function drawRegionsMap() {
+    var data = google.visualization.arrayToDataTable([
+      ['Country', 'Ethnic Tension'],
+      ['Russia', 2],
+      ['USA', 2],
+      ['Northern Corea', 2],
+      ['Germany', 2],
+
+      ['France', 1],
+      ['England', 1],
+    ])
+
+    // https://developers.google.com/chart/interactive/docs/gallery/geochart
+    var options = {
+        // colorAxis: {colors: ['#6f6', '#f66']},
+        colorAxis: {colors: ['#dd6', '#f66']},
+        backgroundColor: '#81d4fa',
+
+        tooltip: {
+            showColorCode: true,
+            textStyle: {
+                color: '#6699cc',
+                fontName: 'monospace',
+                fontSize: '16'
+            },
+        },
+    }
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'))
+
+    google.visualization.events.addListener(chart, 'select', () => {
+        console.log( data.og[chart.getSelection()[0].row].c[0] )
+    })
+
+    chart.draw(data, options)
+}
